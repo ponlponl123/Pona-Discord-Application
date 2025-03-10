4f36f79cf949a50ff0e91b12bc2a5d5cb06c25de
Ponlponl123 <my.devpattarapong@gmail.com>
2025-03-10 18:24:01 +0700

    added search filter api

--- src/server/api/REST/v1/music/search.ts
+++ src/server/api/REST/v1/music/search.ts
@@ -7,7 +7,7 @@ import { ytmusic } from '@/index';
 export async function GET(request: express.Request, response: express.Response) {
   try {
     const { authorization } = request.headers;
-    const { q, is_suggestion } = request.query;
+    const { q, is_suggestion, filter } = request.query;
     if ( !q ) return response.status(400).json({ error: "Missing required parameters" });
     if ( !authorization ) return response.status(HttpStatusCode.Unauthorized).json({error: 'Unauthorized'});
     const tokenType = authorization.split(' ')[0];
@@ -19,7 +19,9 @@ export async function GET(request: express.Request, response: express.Response) 
       const searchSuggestions = await ytmusic.client.getSearchSuggestions(String(q));
       return response.status(HttpStatusCode.Ok).json({message: 'Ok', searchSuggestions: searchSuggestions});
     } else {
-      const searchResult = await YTMusicAPI('GET', `search/${encodeURIComponent(String(q))}`);
+      let URL = `search/${encodeURIComponent(String(q))}`;
+      URL += filter ? `?filter=${filter}` : "";
+      const searchResult = await YTMusicAPI('GET', URL.toString());
       if ( !searchResult ) return response.status(HttpStatusCode.ServiceUnavailable).json({message: 'Service Unavailable'});
       return response.status(HttpStatusCode.Ok).json({message: 'Ok', result: searchResult.data.result});
     }
